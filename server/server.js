f6845649fe333bf3dd582784e5f674762f0fa69b
brandolee15 <b_lee12@hotmail.co.uk>
2021-12-14 18:06:29 +0000

    Added some comments and updated the timestamps

--- server/server.js
+++ server/server.js
@@ -8,42 +8,42 @@ const { finished } = require('stream');
 // var posts = JSON.parse(data);
 // console.log(posts)
 
-const data = require('../client/data')
-var posts = JSON.stringify(data)
+// const data = require('../client/data')
+// var posts = JSON.stringify(data)
 app.use(express.static('../client'))
 
 app.get('/', (req, res) => {
     res.send('Hello World, this is our propaganda blog website!')
 });
 
-app.get('/add/:idpost/:text', addPost);
+// app.get('/add/:idpost/:text', addPost);
 
-function addPost(request, response) {
-    var data = request.params;
-    var idpost = data.idpost;
-    var text = data.text;
-    var reply;
-    if (!text) {
-        reply = {
-            msg: "A blog is required."
-        }
-    } else {
-        posts[idpost] = text;
-        var data = JSON.stringify(posts, null, 2);
-        fs.writeFile('posts.js', data, finished);
+// function addPost(request, response) {
+//     var data = request.params;
+//     var idpost = data.idpost;
+//     var text = data.text;
+//     var reply;
+//     if (!text) {
+//         reply = {
+//             msg: "A blog is required."
+//         }
+//     } else {
+//         posts[idpost] = text;
+//         var data = JSON.stringify(posts, null, 2);
+//         fs.writeFile('posts.js', data, finished);
 
-        function finished(err) {
-            console.log('All good.');
-            reply = {
-                idpost: idpost,
-                text: text,
-                status: "success"
-            }
-            response.send(reply)
-        }
+//         function finished(err) {
+//             console.log('All good.');
+//             reply = {
+//                 idpost: idpost,
+//                 text: text,
+//                 status: "success"
+//             }
+//             response.send(reply)
+//         }
         
-        }
-    }
+//         }
+//     }
 
 
 app.listen(port, console.log(`the sun is listening at http://localhost:${port}`));
