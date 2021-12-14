da85f547e28c74298afb496c364eef97f9f657c1
Marina Ivanova <90510429+wotaque@users.noreply.github.com>
2021-12-14 22:07:34 +0000

    Connect client with server
    Make the client to fetch posts from the server.

--- client/script.js
+++ client/script.js
@@ -1,89 +1,24 @@
-const entryTitle = document.getElementById("entry-title");
-const entryTextbox = document.getElementById("entry-textbox");
-const entryForm = document.getElementById("entry-form");
-var list = document.getElementById('entryList');
-
-
-getAllEntries();
-var numEntries = entries.length;
-
-function saveMessage() {
-    localStorage.setItem('entries', JSON.stringify(entries));
-};
-
-
-function addMessage(title, message) {
-    var entry = {
-        id: numEntries,
-        title: title,
-        message: message,
-        comments: []
-    }
-    entries.push(entry);
-    saveMessage();
-    numEntries++
-    console.log(entries);
-    
-
-    var commentCount = (entry.comments != null) ? entry.comments.length : "0";
-        var html = `
-        <li class="row">
-            <a href="/message.html?${entry.id}">
-            <h4 class="title">
-                ${entry.title}
-            <h4>
-            </a>
-            <div class="bottom>
-                <p class="message">
-                    ${entry.message}
-                </p>
-                <p class="comment-count">
-                    ${commentCount} comments
-                </p>
-            </div>
-        </li>
-        `
-        list.insertAdjacentHTML('afterend', html)
-};
-
-function getAllEntries() {
-
-    for (let entry of entries) {
-        var commentCount = (entry.comments != null) ? entry.comments.length : "0";
-        var html = `
-        <li class="row">
-            <a href="message.html?${entry.id}">
-            <h4 class="title">
-                ${entry.title}
-            <h4>
-            </a>
-            <div class="bottom>
-                <p class="message">
-                    ${entry.message}
-                </p>
-                <p class="comment-count">
-                    ${commentCount} comments
-                </p>
-            </div>
-        </li>
-        `
-        list.insertAdjacentHTML('afterend', html)
-    };
-};
-
-function onEntrySubmit(event) {
-    event.preventDefault();
-    console.log(entryTitle.value)
-    console.log(entryTextbox.value)
-    addMessage(entryTitle.value, entryTextbox.value);
-    clearInputFields();
-    console.log(entries);
+function renderPost(post) {
+    const li = document.createElement('li');
+    const text = document.createTextNode(post.text);
+    li.appendChild(text);
+    return li;
 }
 
-function clearInputFields() {
-    entryTitle.value = "";
-    entryTextbox.value = "";
-  }
+function renderPosts(posts) {
+    container = document.getElementById('posts');
 
-entryForm.addEventListener("submit", onEntrySubmit);
-  
+    posts.forEach(post => {
+        container.appendChild(renderPost(post));
+    });
+}
+
+function reloadPosts() {
+    fetch('http://localhost:3000/')
+        .then(response => response.json())
+        .then(renderPosts);
+}
+
+window.addEventListener('load', function() {
+    reloadPosts();
+});
