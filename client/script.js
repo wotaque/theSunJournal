fe0fef6356b2762e0a2b937affc8ca981b7657c3
Marina Ivanova <90510429+wotaque@users.noreply.github.com>
2021-12-15 00:55:41 +0000

    Support replies

--- client/script.js
+++ client/script.js
@@ -16,8 +16,25 @@ function post(url, json) {
 
 function renderPost(post) {
     const li = document.createElement('li');
+    li.setAttribute('id', 'post-'+post.id);
     const text = document.createTextNode(post.text);
+    const replies = document.createElement('ol');
     li.appendChild(text);
+    li.appendChild(replies);
+    const comment_form = document.createElement('form');
+    const comment_line = document.createElement('textarea');
+    comment_line.setAttribute('name', 'text');
+    const post_id = document.createElement('input');
+    post_id.setAttribute('type', 'hidden');
+    post_id.setAttribute('name', 'post_id');
+    post_id.value = post.id;
+    const send_button = document.createElement('input');
+    send_button.setAttribute('type', 'submit');
+    comment_form.appendChild(post_id);
+    comment_form.appendChild(comment_line);
+    comment_form.appendChild(send_button);
+    comment_form.addEventListener('submit', submitComment);
+    li.appendChild(comment_form);
     return li;
 }
 
@@ -25,14 +42,44 @@ function appendPost(post) {
     postsContainer.appendChild(renderPost(post));
 }
 
-function renderPosts(posts) {
-    posts.forEach(appendPost);
+function submitComment(event) {
+    event.preventDefault();
+    const form = event.target;
+    const post_id = form.querySelector('[name=post_id]').value;
+    const textarea = form.querySelector('textarea');
+    const text = textarea.value;
+    textarea.value = '';
+
+    post(endpoint(post_id+'/reply'), { text: text })
+        .then(response => response.json())
+        .then(appendReply);
+}
+
+function renderReply(reply) {
+    const li = document.createElement('li');
+    const text = document.createTextNode(reply.text);
+    li.appendChild(text);
+    return li;
+}
+
+function appendReply(reply) {
+    const post_id = reply.post_id;
+    container = document.getElementById('post-'+post_id).getElementsByTagName('ol')[0];
+
+    if (container) {
+        container.appendChild(renderReply(reply));
+    }
+}
+
+function renderRoot(data) {
+    data.posts.forEach(appendPost);
+    data.replies.forEach(appendReply);
 }
 
 function reloadPosts() {
     fetch(endpoint())
         .then(response => response.json())
-        .then(renderPosts);
+        .then(renderRoot);
 }
 
 function createPost(text) {
--- server/server.js
+++ server/server.js
@@ -2,8 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000; 
 
-const posts = []
+const posts = {}
 let post_id = 0;
+const replies = []
+let reply_id = 0;
 
 const cors = require('cors');
 app.use(cors());
@@ -11,7 +13,10 @@ app.use(cors());
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.send(posts)
+    res.send({
+        posts: Object.values(posts),
+        replies: replies,
+    })
 });
 
 app.post('/', function(req, res) {
@@ -24,8 +29,30 @@ app.post('/', function(req, res) {
         text: text,
         id: post_id,
     }
-    posts.push(post);
+    posts[post_id] = post;
     res.send(post);
+});
+
+app.post('/:post_id/reply', (req, res) => {
+    const reply_data = req.body;
+    const post_id = req.params.post_id;
+
+    if (!post_id) {
+        throw "No `post_id`";
+    }
+
+    if (!posts.hasOwnProperty(post_id)) {
+        throw "No post with id " + post_id;
+    }
+
+    reply_id++;
+    const reply = {
+        id: reply_id,
+        post_id: post_id,
+        text: reply_data.text,
+    };
+    replies.push(reply);
+    res.send(reply);
 });
 
 app.listen(port, console.log(`the sun is listening at http://localhost:${port}`));
